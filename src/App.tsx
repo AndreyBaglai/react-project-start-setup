b8f1df8aac3b1c728d092ea06008eb06f0083858
Andrey Baglay <andreybaglai@gmail.com>
2024-07-13 16:23:45 +0300

    Fix prettier and eslint bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-green-600 font-semibold">Hello</h1>
+      <h1 className="text-4xl font-semibold text-green-600">Hello</h1>
       <p className="text-3xl font-bold text-sky-400 underline">
         Click on the Vite and React logos to learn more
       </p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@api": "/src/api",
-      "@assets": "/src/assets",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@router": "/src/router",
-      "@routes": "/src/routes",
-      "@myfirebase": "/src/myfirebase",
-      "@zustand": "/src/zustand",
-      "@utils": "/src/utils",
-      "@interfaces": "/src/interfaces",
-      "@enums": "/src/enums",
-      "@hooks": "/src/hooks",
-      "@constants": "/src/constants",
-      "@serviceWorker": "/src/serviceWorker",
-      "@i18n": "/src/i18n",
+      '@api': '/src/api',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@router': '/src/router',
+      '@routes': '/src/routes',
+      '@database': '/src/database',
+      '@store': '/src/store',
+      '@utils': '/src/utils',
+      '@interfaces': '/src/interfaces',
+      '@enums': '/src/enums',
+      '@hooks': '/src/hooks',
+      '@constants': '/src/constants',
+      '@locales': '/src/locales',
     },
   },
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
 });
