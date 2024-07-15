77aa8f0327133cb036cdb21d402c1097c3588c88
Andrey Baglay <andreybaglai@gmail.com>
2024-07-15 20:59:10 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
+import AppRouter from '@router/AppRouter';
+
 function App() {
-  return (
-    <>
-      <h1 className="text-4xl font-semibold text-green-600">Hello</h1>
-      <p className="text-3xl font-bold text-sky-400 underline">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
